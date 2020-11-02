4aabe376e2fb4054ef267d2dbc90c073cd9142bc
Gurkan420 <august.ljungdell@skola.botkyrka.se>
2020-11-02 16:04:04 +0100

    update

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Read instructions. And remove this line.
+function randomInt (n) {
+    return Math.floor(Math.random() * 100)
+
+}
+
+
+const secretNumber = randomInt()
+console.log(secretNumber)
+console.log(secretNumber)
+
+function getUserGuess () {
+    const stringValue = document.getElementById("userInput").value
+    return parseInt(stringValue, 10)
+}
+
+document.addEventListener("keyUp", function (event) {
+    if (event.key === "enter") {
+        const guess = getGuess()
+    }
+
+
+})
+
+function setMessage (userGuess) {
+    if (userGuess > secretNumber) {
+        message.innerHTML = "Too High!";
+   
+    }
+    else if (userGuess < secretNumber) {
+        message.innerHTML = "Too low!";
+    
+        
+    }
+    else { message.innerHTML = "correctGuess!"
+
+
+    }
+clearInput();
+
+
+}
